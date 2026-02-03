51b247b6c50a82e7e3a2edfbe7c2ef2f46e95c61
Master Mind <48193931+iboss21@users.noreply.github.com>
2026-02-02 18:18:29 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -73,8 +73,7 @@
 
 fx_version 'cerulean'
 game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources WILL become incompatible once RedM ships.'
-
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 -- Resource metadata
 name 'lxr-hitmarker'
 author 'iBoss21 / The Lux Empire'
